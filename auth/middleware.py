c623cf71eab1dff3647a792ac7692989e146e96c
Rogerio Hilbert Lima <rogerhil@gmail.com>
2012-06-19 11:24:11 -0300

    Liberate admin area.

--- auth/middleware.py
+++ auth/middleware.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render_to_response
 
-IGPATHS = ['media', 'jsi18n', 'auth']
+IGPATHS = ['media', 'jsi18n', 'auth', 'admin']
 
 class ComingSoonMiddleware(object):
 
