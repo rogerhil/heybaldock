448c74ca63e09f0ffc518a936b0cf27645ae8f57
Rogerio Hilbert Lima <rogerhil@gmail.com>
2012-06-19 10:36:27 -0300

    Admin auto create user signal fix.

--- auth/__init__.py
+++ auth/__init__.py
@@ -8,29 +8,29 @@ from django.db.models import signals
 # Prevent interactive question about wanting a superuser created.  (This code
 # has to go in this otherwise empty "models" module so that it gets processed by
 # the "syncdb" command during database creation.)
-signals.post_syncdb.disconnect(
-    create_superuser,
-    sender=auth_models,
-    dispatch_uid='django.contrib.auth.management.create_superuser')
+signals.post_syncdb.disconnect(create_superuser, sender=auth_models,
+                dispatch_uid='django.contrib.auth.management.create_superuser')
 
 
 # Create our own test user automatically.
 
 def create_testuser(app, created_models, verbosity, **kwargs):
-  if not settings.DEBUG:
-    return
-  try:
-    auth_models.User.objects.get(username='test')
-  except auth_models.User.DoesNotExist:
-    username = 'rogerhil'
-    email = 'rogerhil@gmail.com'
-    password = 'alabama'
-    print '*' * 80
-    print 'Creating test user -- login: %s, password: %s' % (username, password)
-    print '*' * 80
-    assert auth_models.User.objects.create_superuser(username, email, password)
-  else:
-    print 'Test user already exists.'
+    if not settings.DEBUG:
+        return
+    try:
+        auth_models.User.objects.get(username='rogerhil')
+    except auth_models.User.DoesNotExist:
+        username = 'rogerhil'
+        email = 'rogerhil@gmail.com'
+        password = 'alabama'
+        print '*' * 80
+        print 'Creating test user -- login: %s, password: %s' % (username,
+                                                                 password)
+        print '*' * 80
+        assert auth_models.User.objects.create_superuser(username, email,
+                                                         password)
+    else:
+        print 'rogerhil user already exists.'
 
-signals.post_syncdb.connect(create_testuser,
-    sender=auth_models, dispatch_uid='common.models.create_testuser')
+signals.post_syncdb.connect(create_testuser, sender=auth_models,
+                            dispatch_uid='common.models.create_testuser')
