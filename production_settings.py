5289f26ae78b7771703ecc5f9e7b72225651bfa1
Rogerio Hilbert Lima <rogerhil@gmail.com>
2012-06-26 10:31:11 -0300

    Updating database settings in prod settings.

--- production_settings.py
+++ production_settings.py
@@ -1,3 +1,16 @@
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'data.db',                      # Or path to database file if using sqlite3.
+        'USER': 'rogerhil_heybaldock',                      # Not used with sqlite3.
+        'NAME': 'rogerhil_heybaldock',
+        'PASSWORD': 'supertogether',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 DEBUG = True
 
 COMMING_SOON = True
