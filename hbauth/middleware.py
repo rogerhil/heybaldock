f2f5ef4c436af5fcf386af054a295748548d918c
Rogerio Hilbert Lima <rogerhil@gmail.com>
2013-02-03 14:15:23 -0200

    Small fixes.

--- hbauth/middleware.py
+++ hbauth/middleware.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render_to_response
 
-IGPATHS = ['media', 'jsi18n', 'auth', 'admin']
+IGPATHS = ['media', 'jsi18n', 'auth', 'admin', 'music']
 
 class ComingSoonMiddleware(object):
 
